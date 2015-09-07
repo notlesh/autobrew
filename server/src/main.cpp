f8dca7d17b6a25aaf84b283585d8b97c0d1096e1
Stephen Shelton <stephen@stephenashelton.com>
2015-09-07 12:00:31 -0600

    server: Create some request handling logic

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,13 +5,12 @@
 
 #include <fcgiapp.h>
 #include <fcgio.h>
-#include <cgicc/Cgicc.h>
-#include <cgicc/HTTPHTMLHeader.h>
-#include <cgicc/HTMLClasses.h>
 
 #include <roller/core/types.h>
 #include <roller/core/log.h>
 #include <roller/core/util.h>
+
+#include "pid.h"
 
 #define AB_SERVER_FASTCGI_SOCKET "/var/run/ab.socket"
 #define AB_SERVER_FASTCGI_BACKLOG 8
@@ -26,7 +25,15 @@ bool g_appRunning = false;
 void handleSignal( i32 sig ) {
 	Log::i( "sig %d caught, flagging app to stop running", sig );
 	g_appRunning = false;
+	FCGX_ShutdownPending();
 }
+
+// handleRequest
+void handleRequest( FCGX_Request& request );
+
+// test PID controller
+void handleStartPID();
+std::shared_ptr<PID> s_pidController;
 
 // mainNULL
 i32 main( i32 argc, char** argv ) {
@@ -44,7 +51,7 @@ i32 main( i32 argc, char** argv ) {
 
 	Log::setLogLevelMode( LOG_LEVEL_MODE_UNIX_TERMINAL );
 
-	// initialize cgicc
+	// initialize fast cgi
 	printf( "initializing fcgx\n" );
 	int result = FCGX_Init();
 	if ( result ) {
@@ -73,22 +80,118 @@ i32 main( i32 argc, char** argv ) {
 			break;
 		}
 
-		// cgicc::CgiInput input;
-		// cgicc::Cgicc parser( &input );
+		try {
 
-		// TODO: handle request
-		std::string response = "{ response: \"hello\" }";
+			handleRequest( request);
 
-		FCGX_FPrintF( request.out, "Status: %s\r\n", "200 OK" );
-		FCGX_FPrintF( request.out, "Content-Type: application/json; charset=utf-8\r\n" );
-		FCGX_FPrintF( request.out, "Content-Length: %d\r\n", response.size() );
-		FCGX_FPrintF( request.out, "\r\n" );
-		FCGX_PutStr( response.c_str(), response.size(), request.out );
+		} catch( exception& e ) {
+			Log::w( "Caught exception while trying to handle request (ignoring): %s", e.what());
+			continue;
+		}
 
-		FCGX_Finish_r( &request );
-
-		usleep( 3 * 1000 );
 	}
 
 	return 0;
+}
+
+// handleRequest
+void handleRequest( FCGX_Request& request ) {
+
+	std::string scriptName = FCGX_GetParam("SCRIPT_NAME", request.envp);
+	std::string pathInfo = FCGX_GetParam("PATH_INFO", request.envp);
+	std::string requestMethod = FCGX_GetParam("REQUEST_METHOD", request.envp);
+	std::string requestUri = FCGX_GetParam("REQUEST_URI", request.envp);
+
+	Log::i( "handling request uri:    %s", requestUri.c_str());
+	Log::i( "         script name:    %s", scriptName.c_str());
+	Log::i( "         path info:      %s", pathInfo.c_str());
+	Log::i( "         request method: %s", requestMethod.c_str());
+
+	
+	// get last part of URI
+	// TODO: clean this up
+	std::string handlerName = requestUri.substr(4, (requestUri.size() - 4));
+	Log::i( "         request handler name: %s", handlerName.c_str());
+
+	std::string jsonResponse = "{}";
+	i32 responseCode = 200;
+	if (handlerName == "start_pid") {
+
+		try {
+			handleStartPID();
+
+			jsonResponse = "{ \"response\": \"OK\" }";
+			responseCode = 200;
+
+		} catch ( exception& e ) {
+			Log::w( "caught exception in handleStartPID: %s", e.what() );
+
+			jsonResponse = roller::makeString( "{ \"response\": \"Failed to start PID controller\", \"reason\": \"%s\"}",
+					e.what());
+			responseCode = 500;
+		}
+
+	} else if (handlerName == "stop_pid") {
+
+		jsonResponse = "{ \"response\": \"Not implemented yet\" }";
+		responseCode = 500;
+
+	} else if (handlerName == "status") {
+
+		jsonResponse = "{ \"response\": \"All systems go\" }";
+		responseCode = 200;
+
+	} else {
+
+		jsonResponse = "{ \"response\": \"Unrecognized Handler\" }";
+		responseCode = 400;
+	}
+
+	// assemble HTTP response
+	std::string statusResponse;
+	switch (responseCode)
+	{
+		case 200:
+			statusResponse = "200 OK";
+			break;
+
+		case 400:
+			statusResponse = "400 Bad Request";
+			break;
+
+		default:
+			Log::w( "HTTP response code not set, assuming 500" );
+			// intentionally fall through
+
+		case 500:
+			statusResponse = "500 Internal Server Error";
+			break;
+	}
+
+	Log::i( "Sending response.." );
+	FCGX_FPrintF( request.out, "Status: %s\r\n", statusResponse.c_str() );
+	FCGX_FPrintF( request.out, "Content-Type: application/json; charset=utf-8\r\n" );
+	FCGX_FPrintF( request.out, "Content-Length: %d\r\n", jsonResponse.size() );
+	FCGX_FPrintF( request.out, "\r\n" );
+	FCGX_PutStr( jsonResponse.c_str(), jsonResponse.size(), request.out );
+
+	FCGX_Finish_r( &request );
+}
+
+void handleStartPID() {
+
+	if ( s_pidController ) {
+		throw RollerException( "Can't start PID controller; already running" );
+	}
+
+	s_pidController.reset(
+			new PID(
+				15.0f,
+				1.0f,
+				3.0f,
+				30.0f,
+				-100.0f,
+				100.0f));
+
+	// TODO: set up a PID/PWM controller, not just a PID class
 }
