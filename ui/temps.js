2d32b58a7fab395e74f07e6c8a13eee03f088521
Stephen Shelton <stephen@stephenashelton.com>
2015-10-26 21:11:09 -0600

    minor UI tweaks

--- ui/temps.js
+++ ui/temps.js
@@ -1,6 +1,8 @@
 var g_lastTempData = {};
 var g_smoothie = {};
 var g_tempSeries = {};
+var g_numericText = {};
+var g_numericDisplays = {};
 
 var g_settings = {
 	"28.EE9B8B040000": {
@@ -55,6 +57,32 @@ function addProbe(probeId) {
 	var tempSeries = new TimeSeries();
 	window.g_smoothie.addTimeSeries(tempSeries, {lineWidth:2,strokeStyle:color});
 	window.g_tempSeries[probeId] = tempSeries;
+
+	// add numeric display on right side
+	var numericDisplay = document.createElement("p");
+	numericDisplay.style.color = color;
+	numericDisplay.className = "probeNumericDisplay";
+	var numericText = document.createTextNode("");
+	var rightColumn = document.getElementById("right_column");
+
+	numericDisplay.appendChild(numericText);
+	rightColumn.appendChild(numericDisplay);
+
+	window.g_numericDisplays[probeId] = numericDisplay;
+	window.g_numericText[probeId] = numericText;
+}
+
+/**
+ * Resize chart
+ */
+function resizeChart(event) {
+
+	// update canvas width and height relative to parent
+	var div = document.getElementById("chart_main");
+	var canvas = document.getElementById("chart_canvas");
+	var parentStyle = window.getComputedStyle(div, null);
+	canvas.width = parseFloat(parentStyle.width);
+	canvas.height = parseFloat(parentStyle.height);
 }
 
 /**
@@ -68,14 +96,10 @@ function tempsStart() {
 	// create graph canvas and smoothie charts
 	//
 
-	// update canvas width and height relative to parent
-	var div = document.getElementById("chart_main");
-	var canvas = document.getElementById("chart_canvas");
-	var parentStyle = window.getComputedStyle(div, null);
-	canvas.width = parseFloat(parentStyle.width);
-	canvas.height = parseFloat(parentStyle.height);
+	resizeChart();
 
 	// millis per pixel will be (1000 * 60 * 10) / width -- entire width will give us 10 minutes
+	var canvas = document.getElementById("chart_canvas");
 	var mpp = 100;
 	if (canvas.width > 0) {
 		mpp = (1000 * 60 * 30) / canvas.width;
@@ -112,10 +136,18 @@ function tempsStart() {
 			var series = window.g_tempSeries[probeId];
 			series.append(tempData.lastSeen, tempData.tempF);
 
+			var textNode = window.g_numericText[probeId];
+			textNode.nodeValue = "" + tempData.tempF;
+
 		}
 
 		getLatestTempData();
 	}, 1000);
+
+	// add resize listener
+	window.onresize = function(event) {
+		resizeChart();
+	};
 
 }
 
